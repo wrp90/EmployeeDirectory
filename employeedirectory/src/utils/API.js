5290cb7d5f850a14a8b36bf3995bb111bd76d9cc
Will Papp <wpapp90@gmail.com>
2021-05-13 13:57:46 -0500

    can now display search page

--- employeedirectory/src/utils/API.js
+++ employeedirectory/src/utils/API.js
@@ -1,12 +1,12 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const BASEURL = 'https://randomuser.me/api/';
+const BASEURL = 'https://randomuser.me/api/';
 
-// // Export an object with a "search" method that searches the Giphy API for the passed query
-// export default {
-//   search: function(query) {
-//       console.log(query)
-//     return axios.get(BASEURL + query);
-//   }
-// };
+// Export an object with a "search" method that searches the Giphy API for the passed query
+export default {
+  search: function(query) {
+      console.log(query)
+    return axios.get(BASEURL + query);
+  }
+};
 
