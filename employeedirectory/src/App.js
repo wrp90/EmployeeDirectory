5719bba22a1aa31bd56ecca5b014fc0dda46951f
Will <wpapp90@gmail.com>
2022-02-28 17:14:10 -0600

    added login page

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Login from './components/Login';
 import SearchResultContainer from './components/SearchResultContainer';
 import Dashboard from './components/Dashboard';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
