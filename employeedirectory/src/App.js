a13182bee8e9ac9d6e43bb6cb113b82bb9b13c57
Will <wpapp90@gmail.com>
2022-02-28 16:59:15 -0600

    added basic dashboard

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import SearchResultContainer from './components/SearchResultContainer';
+import Dashboard from './components/Dashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-      <SearchResultContainer />
+      <BrowserRouter>
+        <SearchResultContainer />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
