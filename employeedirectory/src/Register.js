4537923d52632095af3c9f2f280bf25a54444edb
Will <wpapp90@gmail.com>
2022-08-24 17:55:20 -0500

    started registration page

---
+++ employeedirectory/src/Register.js
@@ -0,0 +1,77 @@
+import { useRef, useState, useEffect } from "react";
+
+//username and password criteria 
+const USER_REGEX = /^[a-zA- Z] [a-zA-Z0-9-_] {3, 23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]) . {8,24}$/;
+
+const Register = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [PwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchedPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user);
+        console.log(result);
+        console.log(user);
+        setValidName(result);
+    }, [user])
+
+    useEffect(() => {
+        const result = PWD_REGEX.test(pwd);
+        console.log(result);
+        console.log(pwd);
+        setValidPwd(result);
+        const match = pwd === matchPwd;
+        setValidMatch(match);
+    }, [pwd, matchPwd])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd, matchPwd])
+
+    return (
+        <section>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}
+                aria-live="assertive" >{errMsg}</p>
+            <h1>Register</h1>
+            <form>
+                <lable htmlFor="username">
+                    Username:
+                </lable>
+                <input
+                    type="text"
+                    id="username"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required
+                    aria-invalid={validName ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}
+                />
+            </form>
+        </section>
+    )
+}
+
+export default Register
